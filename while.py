285ba194d1455397c7eefa0e63b0250fc71051e7
lordLegal <beneder.martin@gmail.com>
2021-03-20 14:51:13 +0100

    Update while.py

--- while.py
+++ while.py
@@ -1,11 +1,8 @@
 import time
-import urllib.request
 
-http = urllib.request.urlopen("https://www.google.com/")
-c = http.read()
+
 print("Programm has started")
 while True:
-    print(c)
     t = time.strftime('%M')
-    time.sleep(60)
+    time.sleep(30)
     print(f'das ist ein Test Plugin {t}')
